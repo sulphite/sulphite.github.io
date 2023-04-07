ae6cfb052eb3a9969d3901287d0a90cb4ec334a2
Janett Gerrard <mikanchik@gmail.com>
2023-04-07 17:58:40 +0100

    install framer-motion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
+import { motion, useScroll } from "framer-motion"
 import './App.css';
 import Navbar from "./components/Navbar"
+
 
 function App() {
   return (
@@ -8,8 +10,11 @@ function App() {
       <Navbar />
       {/* about section */}
       <div className='container'>
-        <div className='project-container under fixed' id="donut">donut project</div>
-        <div className='project-container under fixed' id="aoy">project</div>
+        <motion.div className='project-container fixed' id="donut"
+          animate={{rotate: "10deg", x: 0}}
+          transition={{ ease: "easeOut", duration: 0.2, x: { duration: 0.1} }}
+          whileHover={{scale: 1.1, x: 100}} />
+        <div className='project-container fixed' id="aoy">project</div>
         <div className='project-container' id="gipf">project</div>
       </div>
     </div>
