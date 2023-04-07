51e3dac51ae422e8f78e91267c105aa63ad761d3
Janett Gerrard <mikanchik@gmail.com>
2023-04-07 14:07:36 +0100

    add nav styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <h3>jfgerrard</h3>
+      <ul className="nav--links">
+        <li>About</li>
+        <li>Projects</li>
+      </ul>
+    </nav>
+  )
+}
