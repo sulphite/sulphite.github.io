4135ab70ba7d386936185d9d00a87ac80620f0e0
Janett Gerrard <mikanchik@gmail.com>
2023-04-07 17:36:38 +0100

    getting project elements to stack

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      {/* about section */}
+      <div className='container'>
+        <div className='project-container under fixed' id="donut">donut project</div>
+        <div className='project-container under fixed' id="aoy">project</div>
+        <div className='project-container' id="gipf">project</div>
+      </div>
     </div>
   );
 }
