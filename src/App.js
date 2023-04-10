541dacff2a6cc222b3c83f5ed0756b4aa7c492dc
Janett Gerrard <mikanchik@gmail.com>
2023-04-10 10:47:14 +0100

    convert project cards into motion divs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import { motion, useScroll } from "framer-motion"
 import './App.css';
 import Navbar from "./components/Navbar"
+import About from "./components/About"
 
 
 function App() {
@@ -14,10 +15,20 @@ function App() {
           className='project-container fixed'
           id="donut"
           animate={{rotate: "10deg", x: 0}}
-          transition={{ ease: "easeOut", duration: 0.2, x: { duration: 0.1} }}
-          whileHover={{x: -100, rotate: "15deg"}}></motion.div>
-        <div className='project-container fixed' id="aoy">project</div>
-        <div className='project-container' id="gipf">project</div>
+          transition={{ ease: "easeOut", duration: 0.2, x: { duration: 0.2} }}
+          whileHover={{x: -100, rotate: "15deg"}}>donut project</motion.div>
+        <motion.div
+          className='project-container fixed'
+          id="aoy"
+          animate={{rotate: "-10deg", x: 30, y: 0 }}
+          transition={{ ease: "easeOut", duration: 0.2, x: { duration: 0.2} }}
+          whileHover={{x: 120, y: 20,rotate: "-15deg"}}>aoy project</motion.div>
+        <motion.div
+          className='project-container'
+          id="gipf"
+          animate={{rotate: "1deg", x: 0, y: 0 }}
+          transition={{ ease: "easeOut", duration: 0.2, x: { duration: 0.2} }}
+          whileHover={{x: 0, y: 0,rotate: "5deg"}}>gipf project</motion.div>
       </div>
     </div>
   );
