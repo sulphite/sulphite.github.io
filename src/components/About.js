21430df15a3af39dff5c84e6db468df2b5671d60
Janett Gerrard <mikanchik@gmail.com>
2023-04-22 12:58:27 +0100

    changed z-index order of elements and applied new ref to second item

--- src/components/About.js
+++ src/components/About.js
@@ -7,22 +7,27 @@ export default function About() {
   const isInView = useInView(ref)
 
   useEffect(() => {
-    console.log("in view:", isInView)
-    const nextItem = document.getElementById("gipf")
+    console.log("about in view:", isInView)
+    const nextItem = document.getElementById("donut")
+    // when gipf element out of view, getnew element and apply
     isInView ? nextItem.classList.add("fixed") : nextItem.classList.remove("fixed")
 
   }, [isInView])
 
   return (
-    <div className="about" id="about" ref={ref}>
-      <img src={pro} />
-      <div className="about--infos">
-        <h1>ABOUT DIV HERE</h1>
-        <p>
-          some example text i'm a full stack developer
-          blah blah blah blah blah
-        </p>
+    <div>
+
+      <div className="about" id="about" ref={ref}>
+        <img src={pro} />
+        <div className="about--infos">
+          <h1>ABOUT DIV HERE</h1>
+          <p>
+            some example text i'm a full stack developer
+            blah blah blah blah blah
+          </p>
+        </div>
       </div>
+
     </div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,11 @@
 import { motion, useScroll, useAnimation, useInView } from "framer-motion"
+import { useEffect, useRef } from "react"
 
 
 export default function Projects() {
+  const gipf_ref = useRef(null)
+  const gipfInView = useInView(gipf_ref)
+
   const donut = {
     init: {x: 1000},
     anim: {rotate: "10deg", x: 0, transition: {type: "spring", duration: 0.6, ease: "easeOut"}},
@@ -20,11 +24,21 @@ export default function Projects() {
     hover: {x: -100, y: 70, rotate: "-3deg", transition: {ease: "easeOut", duration: 0.2}}
   }
 
+  useEffect(() => {
+    const nextItem = document.getElementById("aoy")
+    gipfInView ? nextItem.classList.add("fixed") : nextItem.classList.remove("fixed")
+    // setTimeout(() => {
+    //   console.log("gipf in view:", gipfInView)
+
+    //   if(gipfInView) {nextItem.classList.add("fixed")}
+    // }, 2000);
+  }, [gipfInView])
 
 
   return (
     <div className='container' id="projects">
       <motion.div
+        ref={gipf_ref}
         variants={donut}
         className='project-container fixed'
         id="donut"
@@ -40,7 +54,8 @@ export default function Projects() {
         id="aoy"
         initial="init"
         animate="anim"
-        whileHover="hover">aoy project</motion.div>
+        whileHover="hover">aoy project
+      </motion.div>
 
       <motion.div
         variants={gipf}
@@ -48,7 +63,8 @@ export default function Projects() {
         id="gipf"
         initial="init"
         animate="anim"
-        whileHover="hover">gipf project</motion.div>
+        whileHover="hover">gipf project
+      </motion.div>
     </div>
   )
 }
