cbc7d8adbb7b8d42a35041d2f62f27027e97826b
Janett Gerrard <mikanchik@gmail.com>
2023-05-11 19:02:23 +0100

    add image

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ const projectdata = {
     description: ["An airbnb-style marketplace app for donuts, built with Ruby on Rails.","Users can search by location and flavour, upload their own donuts, and arrange pickup times."],
     github: "https://github.com/sulphite/every-bite-counts",
     url: "https://every-bite-counts.herokuapp.com/",
-    image_url: "https://cdn.discordapp.com/attachments/859729768077066300/1105984580210663524/signal-2023-05-10-230704_002.gif",
+    image_url: "/donuts.gif",
     tools: ["Rails", "Heroku", "PostgreSQL", "APIs"],
     animation: {
       init: {opacity: 0},
@@ -30,7 +30,7 @@ const projectdata = {
   "The sitewide search and tagging system was made to be simple to use for those unfamiliar with tech."],
     github: "https://github.com/nikstojkov/AllofYou",
     url: "www.allofyou.art",
-    image_url: "https://placehold.co/400x250",
+    image_url: "/aoy.jpg",
     tools: ["Rails","Hotwire","StimulusJS", "PostgreSQL"],
     animation: {
       init: {opacity: 0},
@@ -45,7 +45,7 @@ const projectdata = {
     description: ["Created at Hack-GPT 2023, a one-day hackathon centered on the power of LLMs. A tongue-in-cheek portrayal of the ease with which LLM safeguards can be subverted, and a reminder of the importance of ethics in AI development."],
     github: "https://github.com/sulphite/givein",
     url: "",
-    image_url: "https://productstars.org/wp-content/themes/saasweb/images/logo-2.png",
+    image_url: "/logo-2.png",
     tools: ["AutoGPT","Next.js","docker"],
     animation: {
       init: {opacity: 0},
